f6d5ca978293e4db04cf0399a0db3bf51ec80365
Majid Hajian <mhadaily@gmail.com>
2020-11-19 10:47:33 +0100

    add dartup partner conf

--- src/components/modules/Communities.tsx
+++ src/components/modules/Communities.tsx
@@ -18,6 +18,23 @@ const Communities = () => {
               <MainTitle title="Supporter" titleStrokeText={'Communities'} />
               <SponsorsLevel title="" list={sponsors} theme={theme} />
               <SponsorshipEmailLink title="Do you want to become part of this amazing conference?" />
+
+              <br />
+
+              <h3>Check out our partner conference DartUp</h3>
+              <br />
+              <p style={{ textAlign: 'center' }}>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer nofollow"
+                  href="https://dartup.ru/eng/"
+                >
+                  <img
+                    alt="dartup"
+                    src="https://d2kq0urxkarztv.cloudfront.net/5baa4a0083c738006135489f/2214994/upload-dd251d0c-fa3b-44f3-bfb4-0ab4ef890fd2.png?w=324&e=webp&cX=43&cY=125&cW=521&cH=141"
+                  />
+                </a>
+              </p>
             </Container>
           </Section>
         )}
